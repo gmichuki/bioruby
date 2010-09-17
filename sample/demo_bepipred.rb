8fb7ec7ed39d8504264df28f42227efdd2a8a132
Sangura Sitati <sangura.sitati@gmail.com>
2010-09-17 17:46:22 +0300

    Add documentation for bepipred demo

--- sample/demo_bepipred.rb
+++ sample/demo_bepipred.rb
@@ -1,4 +1,25 @@
-# Basic test for bepipred class
+# = sample/demo_bepipred.rb - demonstration of Bio::Bepipred
+#
+# Copyright::   Copyright (C) 2010 
+#               Alan Orth <a.orth@cgiar.org>
+# License::     The Ruby License
+#
+# == Description
+#
+# Demonstration of Bio::Bepipred
+#
+# == Requirements
+#
+# you have to be in lib/bio/appl for this to work  
+#
+# == Usage
+# 
+#  $ cd lib/bio/bio
+#
+#  Run this script as follows.
+#
+#  $ ruby ../../../sample/demo_bepipred.rb
+#
 
 require 'bepipred'
 
