7a89b6dad4835785c878c3ca99b947c196348d56
Michael Pedersen <kaal@kaal.dk>
2010-09-17 17:33:44 +0300

    Initial version of bepipred wrapper class. Includes paser and gff3 output functionality (aswell as a dummy loader)
    Note: code is extremely rough - needs to be cleaned up.

--- lib/bio/appl/bepipred.rb
+++ lib/bio/appl/bepipred.rb
@@ -38,7 +38,7 @@ class Bepipred
   autoload :Report, 'bio/appl/bepipred/report'
   
   # Creates a new Bepipred execution wrapper object
-  def initialize(program='bepipred',score_threshold=0.35,file_name)
+  def initialize(program='bepipred',score_threshold=0.35,file_name='')
     @program = program
     @score_threshold = score_threshold
     @file_name = file_name
@@ -79,6 +79,63 @@ class Bepipred
   
   # TODO create a parser class for the ouput
   # parse_results
+  DATAPARSED = false
+  RESULTS = []
+
+  def dummyload()
+    return `/home/kaal/work/ruby/bepipred-1.0b/bepipred </home/kaal/work/ruby/bepipred-1.0b/test/Pellequer.fsa`
+  end
+
+  def parse(input) 
+    input.each do |line| 
+      if (line != "" and not line.split[0] =~ /^\#.*/)
+        temp = line.split(/\s{2,}/)
+        # fix the source and attributes section
+        temp[1] = temp[1].gsub!(" epitope", "")
+        if (temp[6] =~ /\..*/) 
+          temp[6] = nil 
+        else
+          temp[6] = {'Note' => 'E'}
+        end 
+        # generate the result
+        RESULTS << [temp[0], temp[1], 'epitope', temp[2].to_i, temp[3].to_i, temp[4].to_f, nil, nil, temp[6]]
+      end
+    end
+  end
+
+
+  def to_gff3()
+    # Delayed loading and parsing to limit resource-useage
+    if not DATAPARSED
+      parse(dummyload())
+    end
+
+    container = Bio::GFF::GFF3.new()
+
+    # Find unique sequence identifiers.
+    sequence_names = []
+    RESULTS.each { |x|
+      if (sequence_names[sequence_names.length-1] != x[0])
+        sequence_names << x[0]
+      end
+    }
+
+    # Create sequence regions for each sequence identifier
+    sequence_names.each { |seq| 
+      subinput = RESULTS.find_all {|rec| rec[0]==seq}
+      min = subinput.min_by {|x| x[3]}
+      max = subinput.max_by {|x| x[4]}
+      container.sequence_regions << Bio::GFF::GFF3::SequenceRegion.new(seq, min[3], max[4])
+    }
+
+    # Lets generate the individual records for the GFF3 output
+    RESULTS.each { |x|
+      container.records << Bio::GFF::GFF3::Record.new(*x)
+    }
+
+    return container.to_s()
+  end
+
   
  private
  #executes bepipred when called localy
